419802727ba6ecaa54693be2ac1993e95b4e7166
Antony David <antonydavid945@gmail.com>
2025-03-06 14:51:10 +0100

    feat(portals): implement portal on maps (#13)

--- src/game/scenes/Town.tsx
+++ src/game/scenes/Town.tsx
@@ -7,19 +7,22 @@ export class Town extends Scene {
   town: GameObjects.Image;
   title: GameObjects.Text;
   player: GameObjects.Sprite;
+  portal: GameObjects.Image;
+  private portalCollider: Phaser.Geom.Circle;
+  private playerCollider: Phaser.Geom.Circle;
+  private isOverlapping = false;
+  private portalRadius = 30;
+  private playerRadius = 10;
 
   constructor() {
     super("Town");
   }
 
   preload() {
-    console.log("Town preload");
-
     this.add.dom(0, 0, reactToDom(<ChatLayout />));
-    // Load the player spritesheet with correct frame dimensions
     this.load.spritesheet("player-run", "assets/npc/Knight/Run/Run-Sheet.png", {
-      frameWidth: 64, // Update to the correct frame width
-      frameHeight: 64, // Update to the correct frame height
+      frameWidth: 64,
+      frameHeight: 64,
     });
 
     this.load.spritesheet(
@@ -31,6 +34,7 @@ export class Town extends Scene {
       },
     );
 
+    this.load.image("star", "star.png");
     this.load.image("tiles", "assets/tilemaps/tiles/town.png");
     this.load.tilemapTiledJSON("town", "assets/tilemaps/json/town.json");
   }
@@ -44,6 +48,8 @@ export class Town extends Scene {
   updatePositionPlayer(x: number, y: number) {
     if (this.player) {
       this.player.setPosition(x, y);
+      this.updatePlayerCollider();
+      this.checkPortalCollision();
     }
   }
 
@@ -58,19 +64,31 @@ export class Town extends Scene {
       align: "center",
     });
 
-    // Create the player sprite
+    this.portal = this.add.image(730, 352, "star");
+    this.portal.setScale(0.5);
     this.player = this.add.sprite(410, 390, "player-run");
-    this.player.setOrigin(0.5, 0.5); // Center the sprite's origin
+    this.player.setOrigin(0.5, 0.5);
 
-    // Create the running animation
+    // Initialiser les colliders
+    this.portalCollider = new Phaser.Geom.Circle(
+      this.portal.x,
+      this.portal.y,
+      this.portalRadius,
+    );
+    this.playerCollider = new Phaser.Geom.Circle(
+      this.player.x,
+      this.player.y,
+      this.playerRadius,
+    );
+
     this.anims.create({
       key: "run",
       frames: this.anims.generateFrameNumbers("player-run", {
         start: 0,
-        end: 5, // 6 frames (0 to 5)
+        end: 5,
       }),
-      frameRate: 10, // Frames per second
-      repeat: -1, // Loop indefinitely
+      frameRate: 10,
+      repeat: -1,
     });
 
     this.player.setOrigin(0.5, 1);
@@ -87,7 +105,56 @@ export class Town extends Scene {
 
     this.player.anims.play("idle");
 
+    this.tweens.add({
+      targets: this.portal,
+      scale: 0.6,
+      duration: 1000,
+      yoyo: true,
+      repeat: -1,
+      ease: "Sine.easeInOut",
+    });
+
     EventBus.emit("current-scene-ready", this);
+  }
+
+  updatePlayerCollider() {
+    if (this.player && this.playerCollider) {
+      this.playerCollider.x = this.player.x;
+      this.playerCollider.y = this.player.y - this.player.height / 2;
+    }
+  }
+
+  checkPortalCollision() {
+    const isColliding = Phaser.Geom.Intersects.CircleToCircle(
+      this.playerCollider,
+      this.portalCollider,
+    );
+
+    if (isColliding && !this.isOverlapping) {
+      this.isOverlapping = true;
+      this.activatePortal();
+    } else if (!isColliding && this.isOverlapping) {
+      this.isOverlapping = false;
+    }
+  }
+
+  activatePortal() {
+    this.tweens.add({
+      targets: this.portal,
+      scale: 1.5,
+      alpha: 0,
+      duration: 500,
+      onComplete: () => {
+        this.changeScene();
+      },
+    });
+
+    this.cameras.main.flash(500, 255, 255, 255);
+  }
+
+  update() {
+    this.updatePlayerCollider();
+    this.checkPortalCollision();
   }
 
   changeScene() {
