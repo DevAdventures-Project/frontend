26ac84aa1af10209edb98a8f24ce53beaf487c3f
Metololo <93775692+Metololo@users.noreply.github.com>
2025-03-06 09:57:31 +0100

    feat: react to dom into scene

---
+++ src/lib/reactToDom.tsx
@@ -0,0 +1,10 @@
+import Test from "@/components/Test";
+import type { JSX } from "react";
+import ReactDOM from "react-dom/client";
+
+export function reactToDom(comp: JSX.Element): HTMLElement {
+  const container = document.createElement("div");
+  const root = ReactDOM.createRoot(container);
+  root.render(comp);
+  return container;
+}
