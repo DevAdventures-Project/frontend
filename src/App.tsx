9c2d981129552fb9232dbe2e8f9c13aa39bd0b42
vanande <101594646+vanande@users.noreply.github.com>
2025-03-07 04:14:01 +0100

    Add Zone Dungeon (#33)
    Co-authored-by: Baguette42 <nevenlegall@gmail.com>
    Co-authored-by: Baguette <87232192+Baguette42@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { WebSocketContext, socket } from "./contexts/WebSocketContext";
+import { EventBus } from "./game/EventBus";
 import { type IRefPhaserGame, PhaserGame } from "./game/PhaserGame";
+import type { CozyCity } from "./game/scenes/CozyCity";
 import type { Dungeon } from "./game/scenes/Dungeon";
+import { Kanojedo } from "./game/scenes/Kanojedo";
+import type { MainMenu } from "./game/scenes/MainMenu";
 import type { Town } from "./game/scenes/Town";
 
 export default function App() {
+  const [canMoveSprite, setCanMoveSprite] = useState(true);
   const phaserRef = useRef<IRefPhaserGame | null>(null);
-  const walkableScenes = ["Town", "Dungeon"];
+  const [spritePosition, setSpritePosition] = useState({ x: 0, y: 0 });
+  const walkableScenes = ["Town", "Dungeon", "CozyCity", "Kanojedo"];
+
+  const changeScene = () => {
+    if (phaserRef.current) {
+      const scene = phaserRef.current.scene as MainMenu;
+
+      if (scene) {
+        scene.changeScene();
+      }
+    }
+  };
+
+  useEffect(() => {
+    const handlePositionUpdate = (position: { x: number; y: number }) => {
+      setSpritePosition(position);
+    };
+
+    EventBus.on("player-position-updated", handlePositionUpdate);
+
+    return () => {
+      EventBus.off("player-position-updated", handlePositionUpdate);
+    };
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (!phaserRef.current) return;
 
-      let currentScene: Town | Dungeon | null = null;
+      let currentScene: Town | Dungeon | CozyCity | null = null;
 
       if (phaserRef.current.scene) {
         const sceneKey = phaserRef.current.scene.scene.key;
         if (!walkableScenes.includes(sceneKey)) return;
 
-        currentScene = phaserRef.current.scene as Town | Dungeon;
+        currentScene = phaserRef.current.scene as Town | Dungeon | CozyCity;
       }
 
       if (!currentScene || !currentScene.playerMovement) return;
@@ -44,13 +72,13 @@ export default function App() {
     const handleKeyUp = (event: KeyboardEvent) => {
       if (!phaserRef.current) return;
 
-      let currentScene: Town | Dungeon | null = null;
+      let currentScene: Town | Dungeon | CozyCity | null = null;
 
       if (phaserRef.current.scene) {
         const sceneKey = phaserRef.current.scene.scene.key;
         if (!walkableScenes.includes(sceneKey)) return;
 
-        currentScene = phaserRef.current.scene as Town | Dungeon;
+        currentScene = phaserRef.current.scene as Town | Dungeon | CozyCity;
       }
 
       if (!currentScene || !currentScene.playerMovement) return;
@@ -67,13 +95,25 @@ export default function App() {
     };
   }, []);
 
+  const currentScene = (scene: Phaser.Scene) => {
+    setCanMoveSprite(scene.scene.key !== "MainMenu");
+  };
+
   return (
     <div id="app">
       <WebSocketContext.Provider value={socket}>
-        <PhaserGame
-          ref={phaserRef}
-          currentActiveScene={(scene: Phaser.Scene) => {}}
-        />
+        <PhaserGame ref={phaserRef} currentActiveScene={currentScene} />
+        <div>
+          <div>
+            <button className="button" onClick={changeScene} type="button">
+              Change Scene
+            </button>
+          </div>
+          <div className="spritePosition">
+            Sprite Position:
+            <pre>{`{\n  x: ${spritePosition.x}\n  y: ${spritePosition.y}\n}`}</pre>
+          </div>
+        </div>
       </WebSocketContext.Provider>
     </div>
   );
